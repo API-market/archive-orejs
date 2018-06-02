6331733ab677b2709f11e283a1233e22e93d951b
Tyler Kuhn <tyler@wargoats.com>
2018-06-01 17:09:14 -0700

    can decode the wallet keys with user password

--- example/index.js
+++ example/index.js
@@ -8,11 +8,16 @@ const orejs = new Orejs({keyProvider: "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyX
 //orejs.eos.getInfo({}).then(info => console.log(info))
 
 // Create a new EOS wallet...
-const walletName = orejs.createOreWallet('hello').then(walletName => {
+const walletPassword = 'hello'
+orejs.createOreWallet(walletPassword).then(walletName => {
   console.log("Wallet Created:", walletName)
 
   // Get the newly created EOS wallet...
-  orejs.getOreWallet(walletName).then(account => {
-    console.log("Wallet Keys:", account)
+  orejs.getOreWallet(walletName).then(wallet => {
+    console.log("Wallet:", wallet)
+  })
+
+  orejs.unlockOreWallet(walletPassword).then(keys => {
+    console.log("Keys:", keys)
   })
 })
--- src/ore.js
+++ src/ore.js
@@ -1,17 +1,19 @@
 const {Keystore, Keygen} = require('eosjs-keygen')
 const base32 = require('base32.js')
+const CryptoJS = require("crypto-js");
 
 const ACCOUNT_NAME_MAX_LENGTH = 12
 
 function generateAccountName(encoding = {type: 'rfc4648', lc: true}){
   // account names are generated based on the current unix timestamp
-  // account names MUST be base32 encoded, and be > 13 characters, in compliance with the EOS standard
+  // account names MUST be base32 encoded, and be < 13 characters, in compliance with the EOS standard
   // encoded timestamps are trimmed from the left, to retain enough randomness for multiple per second
   const encoder = new base32.Encoder(encoding);
   const timestamp = Date.now().toString()
   const buffer = Buffer.from(timestamp)
   const encodedTimestamp = encoder.write(buffer).finalize();
   const idx = encodedTimestamp.length - ACCOUNT_NAME_MAX_LENGTH
+
   return encodedTimestamp.substr(idx, ACCOUNT_NAME_MAX_LENGTH)
 }
 
@@ -27,15 +29,30 @@ async function createOreAccount(publicKey, oreAccountName = generateAccountName(
   return oreAccountName
 }
 
+function encrypt(unencrypted, password) {
+  let encrypted = CryptoJS.AES.encrypt(unencrypted, password);
+
+  return encrypted
+}
+
+function decrypt(encrypted, password) {
+  let bytes = CryptoJS.AES.decrypt(encrypted.toString(), password);
+  let unencrypted = bytes.toString(CryptoJS.enc.Utf8);
+
+  return unencrypted
+}
+
 async function createOreWallet(walletPassword) {
   // Generate wallet => new wallet password -> encrypt with userWalletPassword => encryptedWalletPassword
   const keys = await Keygen.generateMasterKeys()
   const oreAccountName = await createOreAccount.bind(this)(keys.publicKeys.active)
 
+  this.encryptedWalletPassword = await encrypt(keys.masterPrivateKey, walletPassword)
+
   return oreAccountName
 }
 
-async function getOreWallet(oreAccountName, walletName = 'default') {
+async function getOreWallet(oreAccountName) {
   //const cpuBalance = this.getCpuBalance(oreAccountName, walletName)
   //const instruments = this.getInstruments(oreAccountName)
   const account = await this.eos.getAccount(oreAccountName)
@@ -44,12 +61,15 @@ async function getOreWallet(oreAccountName, walletName = 'default') {
   //return [walletName, [cpuBalance, instruments]]
 }
 
-function unlockWallet(walletPassword) {
-  // Call eosjs.unlockWallet???? - This should not be necessary on EOS
+async function unlockOreWallet(walletPassword) {
+  const masterPrivateKey = await decrypt(this.encryptedWalletPassword, walletPassword)
+  const keys = Keygen.generateMasterKeys(masterPrivateKey)
+  
+  return keys
 }
 
 module.exports = {
   createOreWallet,
   getOreWallet,
-  unlockWallet
+  unlockOreWallet
 }
