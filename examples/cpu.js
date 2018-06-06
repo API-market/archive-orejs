173f763a5bc50755286bda60653108f4a016fe24
Tyler Kuhn <tyler@wargoats.com>
2018-06-05 18:34:30 -0700

    cpu balance can be retrieved with bounds

--- examples/cpu.js
+++ examples/cpu.js
@@ -1,17 +1,5 @@
-const { Orejs } = require("../src")
-const fs = require('fs')
+const {orejs, walletPassword} = require("./index")
 
-const orejs = new Orejs({keyProvider: "5HzUYWY4nd9Lfe2EW6gnLffLUfKvNgPAPZsd5d7v9dU5wUr4xkw"})
-
-// Error reading contract; https://github.com/EOSIO/eos/issues/3159
-orejs.eos.contract('token.eos2').then(code => {
-  console.log("Actions:", Object.keys(code))
-  orejs.eos.getTableRows({
-         scope: 'token.eos2',
-         code: 'token.eos2',
-         table: 'accounts',
-         json: true
-      }).then(accounts => {
-    console.log("Accounts:", accounts)
-  })
+const balance = orejs.getCpuBalance('token.eos2').then(balance => {
+  console.log("Balance:", balance)
 })
