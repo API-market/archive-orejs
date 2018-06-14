59e26c3583c0a2886baf49dd50a22e7107a05c2d
Tyler Kuhn <tyler@wargoats.com>
2018-06-14 15:55:01 -0700

    keep github issue reference

--- examples/eos/eos_contract_create.js
+++ examples/eos/eos_contract_create.js
@@ -19,5 +19,6 @@ const {orejs} = require("./index")
   let code = await orejs.eos.setcode(accountName, 0, 0, wasm) // @returns {Promise}
   // TODO Figure out why using a contract ABI's published via eosjs does not work,
   // However, contracts published via cleos seem to work just fine
+  // https://github.com/EOSIO/eosjs/issues/110
   //let abi = await orejs.eos.setabi(accountName, JSON.parse(abi)) // @returns {Promise}
 })()
