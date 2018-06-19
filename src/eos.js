9a75e2cc92004b5a6a18fb3cd880140983b5383a
Tyler Kuhn <tyler@wargoats.com>
2018-06-19 16:40:30 -0700

    trying to DRY things up

--- src/eos.js
+++ src/eos.js
@@ -1,23 +1,17 @@
 const BigNumber = require("bignumber.js")
 
-// Find rows in a table
-async function find(contractName, tableName, lowerBound, upperBound, limit = 1, json = true) {
-  const records = await this.eos.getTableRows({
+// Find one row in a table
+async function findOne(contractName, tableName, tableKey) {
+  let results = await this.eos.getTableRows({
     code: contractName.toString(),
-    json: json,
-    limit: limit,
-    lower_bound: lowerBound.toString(),
+    json: true,
+    limit: 1,
+    lower_bound: tableKey.toString(),
     scope: contractName.toString(),
     table: tableName.toString(),
-    upper_bound: upperBound.toString()
+    upper_bound: tableKey.plus(1).toString()
   })
-  return records.rows
-}
-
-// Find one row in a table
-async function findOne(contractName, tableName, tableKey, json = true) {
-  const rows = await this.find(contractName, tableName, tableKey, tableKey.plus(1), 1, json)
-  return rows[0]
+  return results.rows[0]
 }
 
 // Transform account names from base32 to their numeric representations
@@ -25,75 +19,65 @@ function tableKey(oreAccountName) {
   return new BigNumber(this.eos.format.encodeName(oreAccountName, false))
 }
 
-async function getTableRowsPage(params, page) {
-  let code = params.code;
-  let scope = params.scope || params.code;
-  let table = params.table;
-  let page_size = params.page_size || 20;
-
-  let resp = await this.eos.getTableRows({
-    code: code,
-    scope: scope,
-    table: table,
-    lower_bound: page * page_size,
-    upper_bount: ((page + 1) * page_size) + 1,
-    json: true,
-  });
+async function getTableRowsPage(params, page = 0, page_size = 20) {
+  params = { ...params,
+    json: params.json || true,
+    lower_bound: params.lower_bound || page * page_size,
+    scope: params.scope || params.code,
+    upper_bound: params.upper_bound || ((page + 1) * page_size) + 1
+  }
+  let resp = await this.eos.getTableRows(params)
 
   return resp;
 }
 
-
 async function getAllTableRows(params) {
+  let more = true
+  let results = []
+  let page = 0
 
-  let more = true;
-  let results = [];
-  let page = 0;
+  do {
+    let result = await getTableRowsPage.bind(this)(params, page++)
+    more = result.more
+    results = results.concat(result.rows)
+  } while(more)
 
-  do{
-    let result = await getTableRowsPage.bind(this)(params, page++);
-    more = result.more;
-    results = results.concat(result.rows);
-  } while(more);
-
-  return results;
+  return results
 }
 
-async function getAllTableRowsFiltered(params, filter){
-  let result = await getAllTableRows.bind(this)(params);
+async function getAllTableRowsFiltered(params, filter) {
+  let result = await getAllTableRows.bind(this)(params)
 
-  return filterRows(result, filter);
+  return filterRows(result, filter)
 }
 
-function filterRows(rows, filter){
-  if(!filter) return rows;
+function filterRows(rows, filter) {
+  if (!filter) return rows
 
-  let result = [];
+  let result = []
 
-  for(let r in rows) {
-    let row = rows[r];
+  for (let r in rows) {
+    let row = rows[r]
 
-    let fits_filter = true;
+    let fits_filter = true
 
-    if(typeof filter === 'function') {
-      fits_filter = filter(row);
+    if (typeof filter === 'function') {
+      fits_filter = filter(row)
     } else {
       for (let f in filter) {
         if (filter[f] != row[f]) fits_filter = false;
       }
     }
 
-    if(!fits_filter) continue;
+    if (!fits_filter) continue
 
-    result.push(rows[r]);
+    result.push(rows[r])
   }
 
-  return result;
+  return result
 }
 
-
 module.exports = {
-  find,
   findOne,
   tableKey,
   getAllTableRows,
