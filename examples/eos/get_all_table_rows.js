8bacaba3f69a68bcc890b3077d3a80cd6ac74f96
melnx <lyosha@buystand.com>
2018-06-22 14:41:14 -0700

    make getAllTableRows work with non-sequential ids

--- examples/eos/get_all_table_rows.js
+++ examples/eos/get_all_table_rows.js
@@ -2,15 +2,22 @@ const orejs = require("../index.js").orejs()
 
 ;(async function() {
   let allResults = await orejs.getAllTableRows({
-    code: "ore.instr",
-    scope: "ore.instr", //optional, defaults to same value as code
+    code: "instr.ore",
+    scope: "instr.ore", //optional, defaults to same value as code
     table: "tokens",
   })
   console.log(allResults)
 
   let filteredResults = await orejs.getAllTableRowsFiltered({
-    code: "ore.instr",
+    code: "instr.ore",
     table: "tokens",
   }, {owner: 'apiuser'})
   console.log(filteredResults)
+
+  var customKeyResults = await getAllTableRowsFiltered({
+    code: "instr.ore",
+    table: "accounts",
+  }, null, "owner" );
+  console.log(customKeyResults);
+
 })()
