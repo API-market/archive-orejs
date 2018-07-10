cabd7c846c24b8eacf7f2a0dd1b3b8133bf71941
Surabhi Lodha <surabhil@usc.edu>
2018-07-10 15:46:31 -0700

    remove unused function

--- examples/ore/usagestats.js
+++ examples/ore/usagestats.js
@@ -1,10 +1,5 @@
 let orejs = require("../index").orejs()
 
-async function connectAs(accountName, accountKey) {
-    // Reinitialize the orejs library, with permissions for the current account...
-    orejs = require("../index").orejs(accountName, accountKey)
-  }
-
 ;(async function () {
     const totalUsage = await orejs.getRightStats("io.hadron.contest-2018-07")
     console.log(totalUsage)
