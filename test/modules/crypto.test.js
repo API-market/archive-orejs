3eab99790a7dc736b7660cb0513f6aafa05cb8f0
Tyler Kuhn <tyler@wargoats.com>
2018-07-30 12:25:15 +0200

    test crypto module

---
+++ test/modules/crypto.test.js
@@ -0,0 +1,22 @@
+const crypto = require('../../src/modules/crypto.js')
+
+describe("encryption/decryption of private keys with wallet passwords", () => {
+  const privateKey = "5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"
+  const walletPassword = "SimpleWalletPassword"
+  const encrypted = crypto.encrypt(privateKey, walletPassword)
+
+  test("returns an encrypted string", () => {
+    expect(encrypted.toString()).toEqual(expect.not.stringContaining(privateKey))
+  })
+
+  test("returns the original privateKey", () => {
+    const decrypted = crypto.decrypt(encrypted, walletPassword)
+    expect(decrypted.toString()).toMatch(privateKey)
+  })
+
+  test("does not return privateKey with a bad password", () => {
+    const badPassword = "BadPassword"
+    const decrypted = crypto.decrypt(encrypted, badPassword)
+    expect(decrypted.toString()).not.toMatch(privateKey)
+  })
+})
