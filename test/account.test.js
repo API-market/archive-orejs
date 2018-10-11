df3bf1a7d740503fc2e5375e0e178f0d97dcdce9
Tyler Kuhn <tyler@wargoats.com>
2018-10-10 17:37:05 -0700

    fix the busted test suite

--- test/account.test.js
+++ test/account.test.js
@@ -5,7 +5,9 @@ const ecc = require('eosjs-ecc');
 const {
   constructOrejs,
   mockGetAccount,
-  mockTransaction,
+  mockGetInfo,
+  mockGetBlock,
+  mockGetTransaction,
 } = require('./helpers/orejs');
 
 describe('account', () => {
@@ -18,24 +20,35 @@ describe('account', () => {
   describe('createOreAccount', () => {
     let spyTransaction;
     let spyAccount;
+    let spyInfo;
+    let spyBlock;
+    let transaction;
+    let info;
+    let block;
     beforeEach(() => {
       mockGetAccount(orejs);
-      mockTransaction(orejs);
+      transaction = mockGetTransaction(orejs);
+      info = mockGetInfo(orejs);
+      block = mockGetBlock(orejs, { block_num: info.head_block_num, transactions: [{ trx: { id: transaction.transaction_id } }] });
       spyTransaction = jest.spyOn(orejs.eos, 'transaction');
       spyAccount = jest.spyOn(orejs.eos, 'getAccount');
+      spyInfo = jest.spyOn(orejs.eos, 'getInfo');
+      spyBlock = jest.spyOn(orejs.eos, 'getBlock');
     });
 
     test('returns a new account', async () => {
       const account = await orejs.createOreAccount(WALLET_PASSWORD, ORE_OWNER_ACCOUNT_KEY);
       expect(spyTransaction).toHaveBeenNthCalledWith(2, expect.any(Function));
       expect(spyAccount).toHaveBeenCalledWith(expect.any(String));
+      expect(spyInfo).toHaveBeenCalledWith({});
+      expect(spyBlock).toHaveBeenCalledWith(block.block_num);
       expect(account).toEqual({
         verifierAuthKey: expect.any(String),
         verifierAuthPublicKey: expect.any(String),
         oreAccountName: expect.stringMatching(/[a-z1-5\.]{12}/),
         privateKey: expect.any(String),
         publicKey: expect.any(String),
-        transaction: expect.any(Function),
+        transaction: transaction,
       });
       expect(ecc.privateToPublic(orejs.decrypt(account.privateKey, WALLET_PASSWORD))).toEqual(account.publicKey);
       expect(ecc.privateToPublic(account.verifierAuthKey)).toEqual(account.verifierAuthPublicKey);
