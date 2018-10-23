b5155b9172be54e9443dc88050bf9b2b1dfe06c1
Tyler Kuhn <tyler@wargoats.com>
2018-10-23 11:14:18 -0700

    refactor tests, as well

--- test/instrument.test.js
+++ test/instrument.test.js
@@ -10,7 +10,6 @@ const {
 } = require('./helpers/fetch');
 const {
   constructOrejs,
-  mockContract
 } = require('./helpers/orejs');
 
 describe('instrument', () => {
@@ -18,29 +17,6 @@ describe('instrument', () => {
 
   beforeAll(() => {
     orejs = constructOrejs();
-  });
-
-  describe('createVoucherInstrument', () => {
-    let contract;
-    let offerId;
-    let offerTemplate;
-    let overrideVoucherId;
-    let options;
-
-    beforeEach(() => {
-      offerId = 1;
-      offerTemplate = '';
-      overrideVoucherId = 0;
-      options = {
-        authorization: `${ORE_OWNER_ACCOUNT_NAME}@active`,
-      };
-      contract = mockContract();
-    });
-
-    test('returns', async () => {
-      await orejs.createVoucherInstrument(ORE_OWNER_ACCOUNT_NAME, ORE_TESTA_ACCOUNT_NAME, offerId);
-      expect(contract.licenseapi).toHaveBeenCalledWith(ORE_OWNER_ACCOUNT_NAME, ORE_TESTA_ACCOUNT_NAME, offerId, offerTemplate, overrideVoucherId, options);
-    });
   });
 
   describe('findInstruments', () => {
@@ -163,14 +139,6 @@ describe('instrument', () => {
         const right = await orejs.getRight(instrument, rightName);
         expect(right).toEqual(undefined);
       });
-    });
-  });
-
-  describe('signVoucher', () => {
-    test('signs a voucher', async () => {
-      const voucherId = 0;
-      const sig = await orejs.signVoucher(voucherId);
-      expect(sig.toString()).toEqual('SIG_K1_K7SnTcWTVuatvRepJ6vmmiHPEh3WWEYiVPB1nD9MZ3LWz91yUxR5fUWmSmNAAP9Dxs2MeKZuDUFoEVfBiKfRozaG2FzfvH');
     });
   });
 });
