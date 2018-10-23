381f2a040fc01156b13e5e6c7bddca35d1fcc00d
Surabhi Lodha <surabhil@usc.edu>
2018-10-23 15:01:11 -0700

    udpate publish api and isActive

--- src/apimarket/offer.js
+++ src/apimarket/offer.js
@@ -6,16 +6,22 @@ async function createOfferInstrument(oreAccountName, offerData, confirm = false)
     authorization: `${oreAccountName}@active`,
   };
   const contract = await this.eos.contract(APIM_CONTRACT_NAME, options);
+
   if (confirm) {
-    return this.awaitTransaction(() => contract.publishapi(oreAccountName, offerData.issuer, offerData.security_type,
-      offerData.mutability, offerData.api_params, offerData.additional_url_params,
-      offerData.parameter_rules, offerData.instrument_template, offerData.start_time,
-      offerData.end_time, offerData.override_offer_id, options));
+    return this.awaitTransaction(() => contract.publishapi(oreAccountName, offerData.issuer, offerData.api_voucher_license_price_in_cpu,
+      offerData.api_voucher_lifetime_in_seconds, offerData.api_voucher_start_date, offerData.api_voucher_end_date,
+      offerData.api_voucher_valid_forever, offerData.api_voucher_mutability, offerData.api_voucher_security_type,
+      offerData.right_params, offerData.api_voucher_parameter_rules,
+      offerData.offer_mutability, offerData.offer_security_type, offerData.offer_template,
+      offerData.offer_start_time, offerData.offer_end_time, offerData.offer_override_id, options));
   }
-  contract.publishapi(oreAccountName, offerData.issuer, offerData.security_type,
-    offerData.mutability, offerData.api_params, offerData.additional_url_params,
-    offerData.parameter_rules, offerData.instrument_template, offerData.start_time,
-    offerData.end_time, offerData.override_offer_id, options);
+
+  contract.publishapi(oreAccountName, offerData.issuer, offerData.api_voucher_license_price_in_cpu,
+    offerData.api_voucher_lifetime_in_seconds, offerData.api_voucher_start_date, offerData.api_voucher_end_date,
+    offerData.api_voucher_valid_forever, offerData.api_voucher_mutability, offerData.api_voucher_security_type,
+    offerData.right_params, offerData.api_voucher_parameter_rules,
+    offerData.offer_mutability, offerData.offer_security_type, offerData.offer_template,
+    offerData.offer_start_time, offerData.offer_end_time, offerData.offer_override_id, options);
   return this;
 }
 
