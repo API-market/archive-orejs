994ee9953cdabbe995a5806e14892f405fa230a0
Surabhi Lodha <surabhil@usc.edu>
2018-11-20 13:51:30 -0800

    add api_voucher_additional_url_parms and voucher_encrypedt_by to licenseapi

--- src/apimarket/voucher.js
+++ src/apimarket/voucher.js
@@ -2,7 +2,7 @@ const APIM_CONTRACT_NAME = 'manager.apim';
 
 const ecc = require('eosjs-ecc');
 
-function createVoucherInstrument(creator, buyer, offer_id, override_voucher_id = 0, offer_template = '', confirm = false) {
+function createVoucherInstrument(creator, buyer, offer_id, override_voucher_id = 0, offer_template = '', api_voucher_additional_url_params = [], voucher_encrypted_by = '', confirm = false) {
   // Exercise an offer to get a voucher
   // overrideVoucherId is passed in to specify the voucher id for the new voucher. If its value is 0, then the voucher id is auto generated
   // either offerTemplate or offerId could be passed in to get a voucher for that offer.
@@ -21,7 +21,9 @@ function createVoucherInstrument(creator, buyer, offer_id, override_voucher_id =
       buyer,
       offer_id,
       offer_template,
-      override_voucher_id
+      api_voucher_additional_url_params,
+      voucher_encrypted_by,
+      override_voucher_id,
     }
   }];
   if (confirm) {
