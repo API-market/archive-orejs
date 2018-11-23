b0f61f5515b158be59a61a6de99a25fac2b832ed
Tray Lewin <tray.lewin@live.com>
2018-11-23 09:26:48 -0800

    renamed aikon and apim account names

--- test/setup.js
+++ test/setup.js
@@ -3,13 +3,13 @@ global.fetch = require('jest-fetch-mock');
 // The following account keys are not used in production...
 
 global.ORE_NETWORK_URI = 'http://127.0.0.1:8888';
-global.ORE_OWNER_ACCOUNT_NAME = 'app.apim';
+global.ORE_OWNER_ACCOUNT_NAME = 'app.acnt';
 global.ORE_OWNER_ACCOUNT_KEY = '5...';
 global.ORE_PAYER_ACCOUNT_NAME = 'eosio';
 global.ORE_PAYER_ACCOUNT_KEY = '5...';
-global.ORE_TESTA_ACCOUNT_NAME = 'test1.apim';
+global.ORE_TESTA_ACCOUNT_NAME = 'test1.acnt';
 global.ORE_TESTA_ACCOUNT_KEY = '5...';
-global.ORE_TESTB_ACCOUNT_NAME = 'test2.apim';
+global.ORE_TESTB_ACCOUNT_NAME = 'test2.acnt';
 global.ORE_TESTB_ACCOUNT_KEY = '5...';
 global.WALLET_PASSWORD = '...';
 global.USER_ACCOUNT_ENCRYPTION_SALT = '...';
